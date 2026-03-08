ca379d6db6f2d001cd8be02c773bb211ae1b1859
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-08 12:57:51 +0100

    Update Polidivisible
    Documento la clase entera especificando que hace cada parte de esta

--- Algoritmos456_/src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ Algoritmos456_/src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -3,10 +3,24 @@ package es.iesmz.ed.algoritmes;
 public class Polidivisible {
     private long num;
 
+
+    /**
+     * Constructor de la clase.
+     *
+     * @param num número que se quiere comprobar.
+     * @throws IllegalArgumentException si el número es negativo.
+     */
     public Polidivisible(long num) {
         this.num = num;
     }
 
+
+    /**
+     * Comprueba si el número es polidivisible.
+     *
+     * @return true si el número cumple la propiedad de polidivisibilidad,
+     * false en caso contrario.
+     */
     public boolean esPolidivisible() {
         if (!ValidaDigitos()) {
             return false;
@@ -23,6 +37,13 @@ public class Polidivisible {
         return true;
     }
 
+
+    /**
+     * Comprueba si el número contiene todos los dígitos desde 0
+     * hasta longitud-1 sin repetirse.
+     *
+     * @return true si los dígitos son correctos.
+     */
     private boolean ValidaDigitos() {
 
         String numeroTexto = String.valueOf(num);
