e8544f56c7baabf725a238d88b557103b8ad3033
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-08 12:46:24 +0100

    Update cablejat
    Documento la clase entera cablejat

--- Algoritmos456_/src/es/iesmz/ed/algoritmes/cablejat.java
+++ Algoritmos456_/src/es/iesmz/ed/algoritmes/cablejat.java
@@ -2,14 +2,26 @@ package es.iesmz.ed.algoritmes;
 
 public class cablejat {
     private String[] cables;
-
+    /**
+     * Constructor de la clase.
+     *
+     * @param cables vector que contiene la descripción de los cables.
+     * @throws IllegalArgumentException si el vector es nulo o está vacío.
+     */
     public cablejat(String[] cables) {
         if (cables == null || cables.length == 0) {
             throw new IllegalArgumentException("El vector de cables no puede estar vacío");
         }
         this.cables = cables;
     }
-    
+
+    /**
+     * Comprueba si es posible conectar todos los cables
+     * formando un círculo.
+     *
+     * @return true si el número total de conectores macho
+     * es igual al número total de conectores hembra.
+     */
     public boolean esPotConnectar() {
         int M = 0;
         int H = 0;
