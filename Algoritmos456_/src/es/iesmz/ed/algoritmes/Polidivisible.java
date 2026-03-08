d43be43d9bce0a7ecea69aae4b6d2f8793b5e743
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-08 12:55:48 +0100

    Update Polidivisible
    Creo la funcion esPolidivisible y un metodo auxiliar ValidaDigitos para validar que los digitos que se introducen en el metodo principal son correctos, sino devolvera false.

--- Algoritmos456_/src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ Algoritmos456_/src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -6,4 +6,37 @@ public class Polidivisible {
     public Polidivisible(long num) {
         this.num = num;
     }
+
+    public boolean esPolidivisible() {
+        if (!ValidaDigitos()) {
+            return false;
+        }
+
+        String numeroTexto = String.valueOf(num);
+        for (int i = numeroTexto.length(); i > 0; i--) {
+            long subNumero = Long.parseLong(numeroTexto.substring(0, i));
+
+            if (subNumero % i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean ValidaDigitos() {
+
+        String numeroTexto = String.valueOf(num);
+        int longitud = numeroTexto.length();
+        boolean[] usados = new boolean[10];
+
+        for (int i = 0; i < longitud; i++) {
+            int digito = numeroTexto.charAt(i) - '0';
+
+            if (digito >= longitud || usados[digito]) {
+                return false;
+            }
+            usados[digito] = true;
+        }
+        return true;
+    }
 }
