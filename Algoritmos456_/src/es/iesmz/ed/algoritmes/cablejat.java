4e017b0b39154a93373785083608c9d1287dd289
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-08 12:42:06 +0100

    Update cablejat
    Subimos la funcion esPotConnectar

--- Algoritmos456_/src/es/iesmz/ed/algoritmes/cablejat.java
+++ Algoritmos456_/src/es/iesmz/ed/algoritmes/cablejat.java
@@ -6,4 +6,20 @@ public class cablejat {
     public cablejat(String[] cables) {
         this.cables = cables;
     }
+    public boolean esPotConnectar() {
+        int M = 0;
+        int H = 0;
+        for (int i = 0; i < cables.length; i++) {
+            String cable = cables[i];
+
+            char extremo1 = cable.charAt(0);
+            char extremo2 = cable.charAt(1);
+
+            if (extremo1 == 'M') M++;
+            if (extremo1 == 'H') H++;
+            if (extremo2 == 'M') M++;
+            if (extremo2 == 'H') H++;
+        }
+        return M == H;
+    }
 }
